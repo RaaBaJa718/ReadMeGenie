036958f3cd554ca87dd8c4d879ffa7b27b8cd02b
Ruben <ruben.alston646@gmail.com>
2025-01-27 02:46:48 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,106 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+// Questions array
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license does your project use?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('README.md has been generated!');
+    });
+}
+
+// Function to generate README content
+const generateReadme = (answers) =>
+`# ${answers.title}
+
+## Description
+
+${answers.description}
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## License
+
+This project is licensed under the ${answers.license} license.
+
+## Questions
+
+If you have any questions, you can reach me at:
+
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+`;
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateReadme(answers);
+        writeToFile('README.md', readmeContent);
+    });
+}
+
+// Function call to initialize app
+init();
